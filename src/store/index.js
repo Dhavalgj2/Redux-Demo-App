164aa99928870b89629085f186e6cdbfa1c40909
Dhaval <71810466+Dhavalgj2@users.noreply.github.com>
2023-04-06 14:03:42 +1000

    Final Changes

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./authSlice";
 import counterReducer from "./counterSlice";
+
 const store = configureStore({
   reducer: { counter: counterReducer, auth: authReducer },
 });
